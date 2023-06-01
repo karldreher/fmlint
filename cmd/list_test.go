5c98e76e134af428d87e61aa9884983e5e4f0ca6
Karl Dreher <karldreher@outlook.com>
2023-05-31 20:05:34 -0700

    test: remove unneccessary fixture

--- cmd/list_test.go
+++ cmd/list_test.go
@@ -2,22 +2,7 @@ package cmd
 
 import (
 	"testing"
-
-	"github.com/spf13/cobra"
 )
-
-var testCmd = &cobra.Command{
-	Use:   "test",
-	Short: "Test Command",
-	Long: `This is only a test command and is not added to the root command,
-	outside of the test context.
-	The only purpose for this command is to test the rule-id annotation.`,
-	Run: func(cmd *cobra.Command, args []string) {
-	}}
-
-func init() {
-	rootCmd.AddCommand(testCmd)
-}
 
 // This test is not specific to list.go.  All commands added to this program
 // should have a rule-id annotation.  In the case that a  command does not have
