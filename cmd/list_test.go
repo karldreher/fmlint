176d7ea32f99f9c0a5e4986ab02f7d46f2bd587c
Karl Dreher <karldreher@outlook.com>
2023-06-05 20:05:12 -0700

    test: rename test

--- cmd/list_test.go
+++ cmd/list_test.go
@@ -7,7 +7,7 @@ import (
 // This test is not specific to list.go.  All commands added to this program
 // should have a rule-id annotation.  In the case that a  command does not have
 // a "categorizable" rule, rule-id should be annotated with "none".
-func TestMain(t *testing.T) {
+func Test_rule_id_annotations(t *testing.T) {
 	cmdList := rootCmd.Commands()
 	for _, command := range cmdList {
 		//Print command name
