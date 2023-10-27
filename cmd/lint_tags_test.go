1fb75f37e460f8c60b13a28ed195ece78c6c210f
Karl Dreher <karldreher@outlook.com>
2023-10-26 20:12:47 -0700

    test: move test files according to command

--- cmd/lint_tags_test.go
+++ cmd/lint_tags_test.go
@@ -9,7 +9,7 @@ import (
 // Checks that a file with properly sorted tags passes.
 func Test_passing_checkTags(t *testing.T) {
 
-	if checkTags("../test/tags_pass.md") == false {
+	if checkTags("../test/lint_tags/tags_pass.md") == false {
 		t.Error("Expected tags_pass.md to pass but it failed")
 		t.Fail()
 	}
@@ -20,7 +20,7 @@ func Test_failing_checkTags(t *testing.T) {
 	// Redirect log output to /dev/null to avoid test result pollution.
 	null, _ := os.Open(os.DevNull)
 	log.SetOutput(null)
-	if checkTags("../test/tags_fail.md") == true {
+	if checkTags("../test/lint_tags/tags_fail.md") == true {
 		t.Error("Expected tags_fail.md to fail but it passed")
 		t.Fail()
 	}
