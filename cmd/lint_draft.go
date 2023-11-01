2f32cb1817ead6c4998d27b09cb7c434496ba198
Karl Dreher <karldreher@outlook.com>
2023-10-31 20:46:54 -0700

    feat: lint draft

--- cmd/lint_draft.go
+++ cmd/lint_draft.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
-	"fmt"
+	"bytes"
+	"log"
+	"os"
 
+	"github.com/adrg/frontmatter"
 	"github.com/spf13/cobra"
 )
 
@@ -12,16 +15,37 @@ var draftCmd = &cobra.Command{
 	Annotations: map[string]string{"rule-id": "draft-enabled"},
 	Short:       "Checks that draft is not set to \"true\" for all files.",
 	Long: `Draft mode should not be enabled.  
-For the most part, draft mode should be avoided before content goes into production.
-If draft: false, this lint rule will pass.  
-If draft: true, this lint will trigger a failure.`,
+The check is designed to avoid drafts being enabled prior to the release of the site.  
+If draft: true, this lint rule will trigger a failure.  
+If draft: false, this lint rule will pass.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO
-		fmt.Println("draft called")
+		evaluateRules(checkDraft)
 	},
 }
 
 func init() {
 	// Note, subcommands of "lint" are still cobra.Commands but should be added to lintCmd.
 	lintCmd.AddCommand(draftCmd)
+}
+
+// checkDraft checks if draft: "false" or unset.
+// Returns true if draft is not set to true, otherwise false.
+func checkDraft(file string) bool {
+	var matter struct {
+		Title string `yaml:"title"`
+		Draft bool   `yaml:"draft"`
+	}
+	b, err := os.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rest, err := frontmatter.Parse(bytes.NewReader(b), &matter)
+	if err != nil {
+		log.Println(rest, err)
+	}
+	if matter.Draft == true {
+		log.Printf("Draft mode enabled. {\"file\": %q, \"draft\": %t}", file, matter.Draft)
+		return false
+	}
+	return true
 }
---
+++ test/lint_draft/draft_fail.md
@@ -0,0 +1,5 @@
+---
+name: "The art of uncertainty in content creation"
+draft: true
+---
+Hello World!
