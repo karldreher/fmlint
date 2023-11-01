7ee413d76670d81607ad953f968eb33de4189262
Karl Dreher <karldreher@outlook.com>
2023-10-31 20:49:45 -0700

    test: silence log output

--- cmd/lint_tags_test.go
+++ cmd/lint_tags_test.go
@@ -8,7 +8,9 @@ import (
 
 // Checks that a file with properly sorted tags passes.
 func Test_passing_checkTags(t *testing.T) {
-
+	// Redirect log output to /dev/null to avoid test result pollution.
+	null, _ := os.Open(os.DevNull)
+	log.SetOutput(null)
 	if checkTags("../test/lint_tags/tags_pass.md") == false {
 		t.Error("Expected tags_pass.md to pass but it failed")
 		t.Fail()
